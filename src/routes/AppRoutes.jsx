f0002de5f56f28ee55e807b12b458c8ea11454e4
soufnotfunny <leminhtam31104@gmail.com>
2025-03-04 14:21:25 +0700

    add SideBar

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
+import Layout from "../layout/Layout";
 
 // Import Pages
 import Home from "../pages/Home";
@@ -12,16 +13,20 @@ import NotFound from "../pages/NotFound";
 const AppRoutes = () => {
   return (
     <Routes>
+      <Route element={<Layout />}>
+
       {/* Public Routes */}
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<PublicRoute element={<Login />} />} />
       <Route path="/register" element={<PublicRoute element={<Register />} />} />
+      <Route path="/dashboard" element={<Dashboard />} />
 
       {/* Private Routes */}
       {/* <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} /> */}
 
       {/* Free Routes (Ai cũng xem được) */}
       <Route path="/" element={<Home />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
