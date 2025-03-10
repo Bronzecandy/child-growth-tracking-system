e9b1a56e72e6bc5225b2fd48eed94ca4daf0ca0f
Tran Nam <namtran09022004@gmail.com>
2025-03-10 21:10:32 +0700

    DashboardLayout

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 const PublicRoute = ({ element }) => {
   const isAuthenticated = !!localStorage.getItem("token");
 
-  return isAuthenticated ? <Navigate to="/dashboard" replace /> : element;
+  return isAuthenticated ? <Navigate to="/" replace /> : element;
 };
 
 export default PublicRoute;
