19b045d5b3a63bc17768acab2765e49278deafa4
Tran Nam <namtran09022004@gmail.com>
2025-03-25 23:58:32 +0700

    adjust deploy

--- src/pages/ConsultationsDashboard.jsx
+++ src/pages/ConsultationsDashboard.jsx
@@ -174,14 +174,6 @@ function ConsultationsDashboard() {
                                         </div>
 
                                     </td>
-
-                                    <td className="px-6 py-4">
-                                        <button
-                                            className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                                        >
-                                            View Detail
-                                        </button>
-                                    </td>
                                 </tr>
                             ))
                         )}
